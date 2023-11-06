d4fd9b7f73ba2f28e9f3da341dd9b3541ddc9f86
yhuan <cokekokotea@gmail.com>
2023-11-06 15:03:40 +0800

    # update

--- src/components/ThreeDimensional/Utils/Debug.js
+++ src/components/ThreeDimensional/Utils/Debug.js
@@ -1,11 +1,11 @@
-import * as dat from 'lil-gui'
+import GUI from 'lil-gui'
 
 export default class Debug {
   constructor() {
     this.active = window.location.hash === '#debug'
 
     if (this.active) {
-      this.ui = new dat.GUI()
+      this.ui = new GUI()
     }
   }
 }
